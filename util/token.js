70bf1b69944e26e041e5f2d251d101cd433cfa80
coderlmw <lmwl0113@163.com>
2021-12-28 00:19:02 +0800

    修改注释

--- util/token.js
+++ util/token.js
@@ -1,29 +1,32 @@
 const jwt = require('jsonwebtoken')
 let jwtScrect = 'lmw'
 //登录接口 生成token的方法
-let setToken = function (id) {
-  return new Promise((resolve, reject) => {
-    //expiresln 设置token过期的时间
-    const token = jwt.sign({ id: id }, jwtScrect, { expiresIn: '24h' })
-    resolve(token)
-  })
+let setToken = function ( id ) {
+    return new Promise((resolve, reject) => {
+        //expiresln 设置token过期的时间
+        //{ user_name: user_name, user_id: user_id } 传入需要解析的值（ 一般为用户名，用户id 等）
+        const token = jwt.sign({ id: id }, jwtScrect, { expiresIn: '24h' });
+        resolve(token)
+    })
 }
 //各个接口需要验证token的方法
 let getToken = function (token) {
-  return new Promise((resolve, reject) => {
-    if (!token) {
-      console.log('token是空的')
-      reject({
-        error: 'token 是空的',
-      })
-    } else {
-      var info = jwt.verify(token.split(' ')[1], jwtScrect)
-      resolve(info) //解析返回的值（sign 传入的值）
-    }
-  })
+    return new Promise((resolve, reject) => {
+        if (!token) {
+            console.log('token是空的')
+            reject({
+                error: 'token 是空的'
+            })
+        }
+        else {
+            //第二种  改版后的
+            var info = jwt.verify(token.split(' ')[1], jwtScrect);
+            resolve(info);  //解析返回的值（sign 传入的值）
+        }
+    })
 }
 
 module.exports = {
-  setToken,
-  getToken,
+    setToken,
+    getToken
 }
